8a6c40fe234fffb25e7b7fc1b6815899243e0fef
ABDELRAHIM4 <140072510+ABDELRAHIM4@users.noreply.github.com>
2026-05-02 21:25:34 +0100

    Implement power plant energy prediction model
    Added functionality to load a dataset, train a Random Forest model, and evaluate its performance using mean squared error.

--- app.py
+++ app.py
@@ -10,6 +10,32 @@ st.title("power plant energy predictor")
 
 
 # Load the model
+#pip install ucimlrepo
+from ucimlrepo import fetch_ucirepo 
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import train_test_split
+import joblib
+# fetch dataset 
+combined_cycle_power_plant = fetch_ucirepo(id=294) 
+  
+# data (as pandas dataframes) 
+X = combined_cycle_power_plant.data.features 
+y = combined_cycle_power_plant.data.targets 
+  
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+# build random forest model
+rf = RandomForestRegressor(n_estimators=100, random_state=42, max_depth=10, min_samples_split=5, min_samples_leaf=2, max_features='sqrt')
+rf.fit(X_train, y_train.values.ravel())
+# predict the model
+pred = rf.predict(X_test)
+# evaluate the model
+mse = mean_squared_error(y_test, pred)
+print("mean squared error", mse)
+joblib.dump(rf, "power_plant_model.pkl")
 model = joblib.load("power_plant_model.pkl")
 col1, col2 = st.columns(2)
 with col1:
